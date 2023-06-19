21e0c006e6ce8c60d6d9bd242f31a00a9e1fc0b5
Michael Englehorn <michael@englehorn.com>
2023-06-18 19:12:47 -0500

    Changed build-docker.sh to build production version

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "BUILD_VER: " BUILD_VER
-docker build --progress plain --build-arg RAILS_ENV=development --build-arg NODE_ENV=development --build-arg BUILD_VER=$BUILD_VER --tag k0hax/lab-enginestats:$BUILD_VER --tag k0hax/lab-enginestats:latest .
+docker build --progress plain --build-arg RAILS_ENV=production --build-arg NODE_ENV=production --build-arg BUILD_VER=$BUILD_VER --tag k0hax/lab-enginestats:$BUILD_VER --tag k0hax/lab-enginestats:latest .
 
